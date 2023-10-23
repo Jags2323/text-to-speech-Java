2ec284d98e452d94c495f44086c35eba1c0c2357
JagsP23 <pullapuramjr@gmail.com>
2023-10-23 15:04:24 -0400

    yaml file

--- src/main/java/com/example/texttospeech/controller/TextToSpeechController.java
+++ src/main/java/com/example/texttospeech/controller/TextToSpeechController.java
@@ -1,74 +1,13 @@
-package com.example.texttospeech.controller;
-////
-////import com.google.api.gax.core.FixedCredentialsProvider;
-////import com.google.auth.oauth2.GoogleCredentials;
-////import com.google.auth.oauth2.ServiceAccountCredentials;
-////import com.google.cloud.texttospeech.v1.*;
-////import com.google.protobuf.ByteString;
-////import org.springframework.web.bind.annotation.GetMapping;
-////import org.springframework.web.bind.annotation.RequestMapping;
-////import org.springframework.web.bind.annotation.RequestParam;
-////import org.springframework.web.bind.annotation.RestController;
-////
-////import java.io.FileInputStream;
-////import java.io.FileOutputStream;
-////import java.io.IOException;
-////import java.io.OutputStream;
-////
-////@RequestMapping("/texttospeech")
-////@RestController
-////public class TextToSpeechController {
-////
-////    @GetMapping("")
-////    public String synthesizeTextToSpeech(@RequestParam String text) {
-////        try {
-////            // Set up authentication using your JSON key file
-////            GoogleCredentials credentials = ServiceAccountCredentials.fromStream(
-////                    new FileInputStream("C:\\Users\\jagad\\OneDrive\\Desktop\\text-to-speech\\text-to-speech\\text-to-speech-key.json"));
-////
-////            TextToSpeechClient textToSpeechClient = TextToSpeechClient.create(TextToSpeechSettings.newBuilder().setCredentialsProvider(
-////                    FixedCredentialsProvider.create(credentials)
-////            ).build());
-////
-////            SynthesisInput input = SynthesisInput.newBuilder().setText(text).build();
-////
-////            VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
-////                    .setLanguageCode("en-US")
-////                    .setSsmlGender(SsmlVoiceGender.NEUTRAL)
-////                    .build();
-////
-////            AudioConfig audioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.LINEAR16).build();
-////
-////            SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
-////
-////            // Save the audio content to an MP3 file
-////            try (OutputStream out = new FileOutputStream("output.mp3")) {
-////                ByteString audioContents = response.getAudioContent();
-////                out.write(audioContents.toByteArray());
-////                System.out.println("Audio content written to file \"output.mp3\"");
-////            } catch (IOException e) {
-////                e.printStackTrace();
-////                return "Error saving audio content: " + e.getMessage();
-////            }
-////
-////            textToSpeechClient.close();
-////
-////            return "Audio content saved as 'output.mp3'";
-////        } catch (IOException e) {
-////            e.printStackTrace();
-////            return "Error: " + e.getMessage();
-////        }
-////    }
-////}
-//
 //package com.example.texttospeech.controller;
 //
 //import com.google.api.gax.core.FixedCredentialsProvider;
 //import com.google.auth.oauth2.GoogleCredentials;
 //import com.google.auth.oauth2.ServiceAccountCredentials;
+//import com.google.cloud.bigtable.data.v2.BigtableDataClient;
+//import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
+//import com.google.cloud.bigtable.data.v2.models.*;
 //import com.google.cloud.texttospeech.v1.*;
 //import com.google.protobuf.ByteString;
-//import org.springframework.http.MediaType;
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
@@ -76,112 +15,129 @@ package com.example.texttospeech.controller;
 //
 //import java.io.FileInputStream;
 //import java.io.IOException;
+//import java.util.ArrayList;
 //import java.util.Arrays;
+//import java.util.List;
+//import java.util.UUID;
 //
 //@RequestMapping("/texttospeech")
 //@RestController
 //public class TextToSpeechController {
+//
+//    private final TextToSpeechClient textToSpeechClient;
+//    private final BigtableDataClient bigtableDataClient;
+//
+//    public TextToSpeechController() {
+//        try {
+//            // Set up authentication for Text-to-Speech
+//            GoogleCredentials textToSpeechCredentials = ServiceAccountCredentials.fromStream(
+//                    new FileInputStream("C:\\Users\\jagad\\OneDrive\\Desktop\\text-to-speech\\text-to-speech\\text-to-speech-key.json"));
+//
+//                    textToSpeechClient = TextToSpeechClient.create(
+//                    TextToSpeechSettings.newBuilder().setCredentialsProvider(
+//                            FixedCredentialsProvider.create(textToSpeechCredentials)
+//                    ).build());
+//
+//            // Set up authentication for Bigtable
+//            GoogleCredentials bigtableCredentials = ServiceAccountCredentials.fromStream(
+//                    new FileInputStream("C:\\Users\\jagad\\OneDrive\\Desktop\\text-to-speech\\text-to-speech\\text-to-speech-key.json"));
+//
+//                    BigtableDataSettings bigtableSettings = BigtableDataSettings.newBuilder()
+//                    .setCredentialsProvider(FixedCredentialsProvider.create(bigtableCredentials))
+//                    .setProjectId("awesome-pilot-400117")
+//                    .setInstanceId("text-to-speech")
+//                    .build();
+//            bigtableDataClient = BigtableDataClient.create(bigtableSettings);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("Error initializing clients: " + e.getMessage());
+//        }
+//    }
 //
 //    @GetMapping(value = "", produces = "audio/mpeg")
 //    public byte[] synthesizeTextToSpeech(@RequestParam String text) {
-//        try {
-//            // Setting up authentication using  JSON key file
-//            GoogleCredentials credentials = ServiceAccountCredentials.fromStream(
-//                    new FileInputStream("C:\\Users\\jagad\\OneDrive\\Desktop\\text-to-speech\\text-to-speech\\text-to-speech-key.json"));
+//        // Text-to-Speech logic
+//        SynthesisInput input = SynthesisInput.newBuilder().setText(text).build();
+//        VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
+//                .setLanguageCode("en-US")
+//                .setSsmlGender(SsmlVoiceGender.NEUTRAL)
+//                .build();
+//        AudioConfig audioConfig = AudioConfig.newBuilder()
+//                .setAudioEncoding(AudioEncoding.LINEAR16)
+//                .build();
+//        SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
+//        ByteString audioContents = response.getAudioContent();
 //
-//            TextToSpeechClient textToSpeechClient = TextToSpeechClient.create(TextToSpeechSettings.newBuilder().setCredentialsProvider(
-//                    FixedCredentialsProvider.create(credentials)
-//            ).build());
+//        // Store user input in Bigtable
+//        String tableId = "text-to-speech"; // Your Bigtable Table ID
+//        String rowKey = generateUniqueRowKey();
 //
-//            SynthesisInput input = SynthesisInput.newBuilder().setText(text).build();
+//        RowMutation rowMutation = RowMutation.create(tableId, rowKey);
+//        rowMutation.setCell("text-to-speech", "user_input", Arrays.toString(audioContents.toByteArray()));
 //
-//            VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
-//                    .setLanguageCode("en-US")
-//                    .setSsmlGender(SsmlVoiceGender.NEUTRAL)
-//                    .build();
+//        bigtableDataClient.mutateRow(rowMutation);
 //
-//            AudioConfig audioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.LINEAR16).build();
-//
-//            SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
-//
-//            // Return the audio content as a byte array
-//            ByteString audioContents = response.getAudioContent();
-//            textToSpeechClient.close();
-//
-//            return audioContents.toByteArray();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            throw new RuntimeException("Error: " + e.getMessage());
-//        }
+//        return audioContents.toByteArray();
 //    }
-//}
-
-//package com.example.texttospeech.controller;
 //
-//import com.google.api.gax.core.FixedCredentialsProvider;
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.auth.oauth2.ServiceAccountCredentials;
-//import com.google.cloud.texttospeech.v1.*;
-//import com.google.protobuf.ByteString;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
+//    @GetMapping("/listdata")
+//    public List<String> listDataFromBigtable() {
+//        List<String> dataEntries = new ArrayList<>();
 //
-//import java.io.FileInputStream;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//import java.io.OutputStream;
-//
-//@RequestMapping("/texttospeech")
-//@RestController
-//public class TextToSpeechController {
-//
-//    @GetMapping("")
-//    public String synthesizeTextToSpeech(@RequestParam String text) {
 //        try {
-//            // Set up authentication using your JSON key file
-//            GoogleCredentials credentials = ServiceAccountCredentials.fromStream(
-//                    new FileInputStream("C:\\Users\\jagad\\OneDrive\\Desktop\\text-to-speech\\text-to-speech\\text-to-speech-key.json"));
-//
-//            TextToSpeechClient textToSpeechClient = TextToSpeechClient.create(TextToSpeechSettings.newBuilder().setCredentialsProvider(
-//                    FixedCredentialsProvider.create(credentials)
-//            ).build());
-//
-//            SynthesisInput input = SynthesisInput.newBuilder().setText(text).build();
-//
-//            VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
-//                    .setLanguageCode("en-US")
-//                    .setSsmlGender(SsmlVoiceGender.NEUTRAL)
+//            // Define the Bigtable settings
+//            BigtableDataSettings bigtableSettings = BigtableDataSettings.newBuilder()
+//                    .setProjectId("awesome-pilot-400117")
+//                    .setInstanceId("text-to-speech")
 //                    .build();
 //
-//            AudioConfig audioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.LINEAR16).build();
+//            // Create a BigtableDataClient
+//            BigtableDataClient bigtableDataClient = BigtableDataClient.create(bigtableSettings);
 //
-//            SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
+//            // Define the Bigtable table and column family
+//            String tableId = "text-to-speech"; // Your Bigtable Table ID
+//            String columnFamily = "text-to-speech"; // Your column family name
 //
-//            // Save the audio content to an MP3 file
-//            try (OutputStream out = new FileOutputStream("output.mp3")) {
-//                ByteString audioContents = response.getAudioContent();
-//                out.write(audioContents.toByteArray());
-//                System.out.println("Audio content written to file \"output.mp3\"");
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                return "Error saving audio content: " + e.getMessage();
+//            // Fetch rows from Bigtable
+//            Iterable<Row> rows = bigtableDataClient.readRows(Query.create(tableId));
+//
+//            for (Row row : rows) {
+//                // Process each row and extract the relevant data
+//                for (RowCell cell : row.getCells(Arrays.toString(columnFamily.getBytes()))) {
+//                    // Assuming that the data is stored as a string
+//                    String cellData = new String(cell.getValue().toByteArray());
+//                    dataEntries.add(cellData);
+//                }
 //            }
 //
-//            textToSpeechClient.close();
+//            // Close the Bigtable client when done
+//            bigtableDataClient.close();
 //
-//            return "Audio content saved as 'output.mp3'";
 //        } catch (IOException e) {
 //            e.printStackTrace();
-//            return "Error: " + e.getMessage();
+//            throw new RuntimeException("Error reading data from Bigtable: " + e.getMessage());
 //        }
+//
+//        return dataEntries;
+//    }
+//
+//    private String generateUniqueRowKey() {
+//        // Generate a unique row key using a combination of timestamp and a random UUID
+//        long timestamp = System.currentTimeMillis();
+//        String uniqueId = UUID.randomUUID().toString();
+//        return timestamp + "-" + uniqueId;
 //    }
 //}
-
-
+//
+//
+//
+//
+//
+package com.example.texttospeech.controller;
 
 import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.bigtable.data.v2.BigtableDataClient;
@@ -189,7 +145,6 @@ import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
 import com.google.cloud.bigtable.data.v2.models.*;
 import com.google.cloud.texttospeech.v1.*;
 import com.google.protobuf.ByteString;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -212,22 +167,18 @@ public class TextToSpeechController {
     public TextToSpeechController() {
         try {
             // Set up authentication for Text-to-Speech
-            GoogleCredentials textToSpeechCredentials = ServiceAccountCredentials.fromStream(
-                    new FileInputStream("C:\\Users\\jagad\\OneDrive\\Desktop\\text-to-speech\\text-to-speech\\text-to-speech-key.json"));
+            Credentials textToSpeechCredentials = GoogleCredentials.getApplicationDefault();
 
-                    textToSpeechClient = TextToSpeechClient.create(
+            textToSpeechClient = TextToSpeechClient.create(
                     TextToSpeechSettings.newBuilder().setCredentialsProvider(
                             FixedCredentialsProvider.create(textToSpeechCredentials)
                     ).build());
 
             // Set up authentication for Bigtable
-            GoogleCredentials bigtableCredentials = ServiceAccountCredentials.fromStream(
-                    new FileInputStream("C:\\Users\\jagad\\OneDrive\\Desktop\\text-to-speech\\text-to-speech\\text-to-speech-key.json"));
+            Credentials bigtableCredentials = GoogleCredentials.getApplicationDefault();
 
-                    BigtableDataSettings bigtableSettings = BigtableDataSettings.newBuilder()
+            BigtableDataSettings bigtableSettings = BigtableDataSettings.newBuilder()
                     .setCredentialsProvider(FixedCredentialsProvider.create(bigtableCredentials))
-                    .setProjectId("awesome-pilot-400117")
-                    .setInstanceId("text-to-speech")
                     .build();
             bigtableDataClient = BigtableDataClient.create(bigtableSettings);
 
